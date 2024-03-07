26d1cd41c7e7d9e0134b063411690e50c6df697d
Kerim Nural <krm.ebr@gmail.com>
2024-03-08 02:07:59 +0300

    fetch data control

--- fundamentals/apps.js
+++ fundamentals/apps.js
@@ -1,7 +1,12 @@
-//console.log('hello world')
-//document.write("kerim nural<br>")
-
-//document.write("5.5")
- 
+const apiKey = "72d6b37f4bdf1297c74848427c10a021";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=turkey";
 
 
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json(); 
+
+    console.log(data);
+}
+
+checkWeather();
