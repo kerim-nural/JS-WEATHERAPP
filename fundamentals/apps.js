ebd9e7666839b7dff793966f2daef257a277b437
Kerim Nural <krm.ebr@gmail.com>
2024-03-08 04:27:47 +0300

    changing weather icon source

--- fundamentals/apps.js
+++ fundamentals/apps.js
@@ -1,18 +1,38 @@
 const apiKey = "72d6b37f4bdf1297c74848427c10a021";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=turkey";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+let weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    var data = await response.json(); 
+async function checkWeather(country){
+    const response = await fetch(apiUrl + country + `&appid=${apiKey}`);
+    const data = await response.json(); 
 
     console.log(data);
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".country").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "km/h";
+    //document.querySelector(".weather img").src = "images/" + data.weather[0].main.toLowerCase() + ".png";
+    if (data.weather[0].main === "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main === "Clear") {
+        weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main === "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main === "Mist") {
+        weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main === "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else {
+        weatherIcon = "images/snow.png";
+    } 
 
+    
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value)
+})
